b50b85898b7774ec263cba4330ecf3b29fad91ee
Ray Barrera <ray@raybarrera.com>
2022-01-23 20:22:44 -0800

    added tokens

---
+++ compiler/pkg/lexer/lexer.go
@@ -0,0 +1,3 @@
+package lexer
+
+// Token implementation
--- compiler/pkg/token/token.go
+++ compiler/pkg/token/token.go
@@ -1,3 +1,50 @@
 package token
 
-// Token implementation
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	EOF        = "EOF"
+	Identifier = "Identifier"
+	Type       = "Type"
+
+	//Operators
+	Assign      = "="
+	Inference   = ":="
+	Plus        = "+"
+	Minus       = "-"
+	Bang        = "!"
+	Asterisk    = "*"
+	Slash       = "/"
+	Equality    = "=="
+	NotEqual    = "!="
+	LessThan    = "<"
+	GreaterThan = ">"
+	Scope       = "::"
+
+	//Delimiters
+	Comma     = ","
+	Semicolon = ";"
+	Colon     = ":"
+
+	LeftParen    = "("
+	RightParent  = ")"
+	LeftBrace    = "{"
+	RightBrace   = "}"
+	LeftBracket  = "["
+	RightBracket = "]"
+
+	//Ketwords
+	True   = "True"
+	False  = "False"
+	If     = "If"
+	Else   = "Else"
+	Return = "Return"
+	Struct = "Struct"
+	Trait  = "Trait"
+	Enum   = "Enum"
+)
